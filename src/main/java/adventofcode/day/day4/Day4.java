2376b011059af9df497133a3f350b24e43f1feae
markusdybeck <markus.dybeck@pricerunner.com>
2017-12-04 10:55:42 +0100

    Day4

--- src/main/java/adventofcode/day/day4/Day4.java
+++ src/main/java/adventofcode/day/day4/Day4.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,34 +24,44 @@ public class Day4 {
 
     private static void starOne() throws IOException {
         final List<String> rows = getInput();
-        final Integer answer = rows.stream().mapToInt(row -> {
-            final InputHelper ih = new InputHelper(row);
-            final List<String> passphrase = ih.toStringList(" ");
-            final Set<String> theSet = new HashSet<>(passphrase);
-
-            return passphrase.size() == theSet.size() ? 1 : 0;
-        }).sum();
+        final Integer answer = rows.stream().mapToInt(Day4::validatePassphraseS1).sum();
 
         System.out.println(answer);
     }
 
     private static void starTwo() throws IOException {
         final List<String> rows = getInput();
-        final Integer answer = rows.stream().mapToInt(row -> {
-            final List<String> passphrase = Stream.of(row.split(" "))
-                .map(String::toCharArray)
-                .peek(Arrays::sort)
-                .map(String::new)
-                .collect(Collectors.toList());
-            final Set<String> theSet = new HashSet<>(passphrase);
-
-            return passphrase.size() == theSet.size() ? 1 : 0;
-        }).sum();
+        final Integer answer = rows.stream().mapToInt(Day4::validatePassphraseS2).sum();
 
         System.out.println(answer);
     }
 
-    private static List<String> getInput() throws IOException {
-        return Files.readAllLines(INPUT_PATH);
+    private static Integer validatePassphraseS1(final String row) {
+        final InputHelper ih = new InputHelper(row);
+        final List<String> passphrase = ih.toStringList(" ");
+        final Set<String> theSet = new HashSet<>(passphrase);
+
+        return passphrase.size() == theSet.size() ? 1 : 0;
+    }
+
+    private static Integer validatePassphraseS2(final String row) {
+        final List<String> passphrase = Stream.of(row.split(" "))
+            .map(String::toCharArray)
+            .peek(Arrays::sort)
+            .map(String::new)
+            .collect(Collectors.toList());
+        final Set<String> theSet = new HashSet<>(passphrase);
+
+        return passphrase.size() == theSet.size() ? 1 : 0;
+    }
+
+    private static List<String> getInput() {
+        try {
+            return Files.readAllLines(INPUT_PATH);
+        } catch (final Exception e) {
+            System.out.println("Error while reading file...");
+        }
+
+        return Collections.emptyList();
     }
 }
